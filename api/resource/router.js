b2d62de0b5fe94a4b2ba9966135db0c99bf5b747
JohnGrabowskiIII <johngrabowski97@gmail.com>
2021-08-28 17:03:27 -0400

    Built skeleton resourceRouter, and connected to server

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,15 @@
 // build your `/api/resources` router here
+
+const express = require('express')
+
+const router = express.Router()
+
+router.post('/', (req, res) => {
+    res.status(200).json({message: "Post route's to /api/resources"})
+})
+
+router.get('/', (req, res) => {
+    res.status(200).json({message: "Get routes to /api/resources"})
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -3,7 +3,11 @@ const express = require('express')
 
 const server = express()
 
+const resourceRouter = require('./resource/router')
+
 server.use(express.json())
+
+server.use('/api/resources', resourceRouter)
 
 server.use('/', (req, res) => {
     res.status(200).json({message: "Server is online"})
