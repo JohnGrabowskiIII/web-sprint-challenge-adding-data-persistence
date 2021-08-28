baf3fb6fd12e52472a2ad662bc170676f738f388
JohnGrabowskiIII <johngrabowski97@gmail.com>
2021-08-27 23:35:35 -0400

    Built out create-projects-table migration, tested

--- data/migrations/20210828025936_create-projects-table.js
+++ data/migrations/20210828025936_create-projects-table.js
@@ -1,8 +1,13 @@
 
+const projects = "projects"
+const project_id = "project_id"
+
 exports.up = function(knex) {
-  
+  return knex.schema.createTable(projects, table => {
+    table.increments(project_id)
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists(projects)
 };
