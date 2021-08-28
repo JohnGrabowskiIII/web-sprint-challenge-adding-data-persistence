bf41a1046bacbfde4536c548b56169595db0d879
JohnGrabowskiIII <johngrabowski97@gmail.com>
2021-08-28 16:42:16 -0400

    Built fallback endpoint in index.js, server connected

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // start your server here
 const server = require('./api/server')
 
-const port = process.env.PORT ? process.env.PORT : 5000
+const port = process.env.PORT ? process.env.PORT : 7777
 
 server.listen(port, () => {
     console.log(`\n +★✶ Server running on port ${port} ✶★+ \n`)
