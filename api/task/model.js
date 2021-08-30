8688bde249e9291ee1bdfbb60b5d8bb058174d0a
JohnGrabowskiIII <johngrabowski97@gmail.com>
2021-08-30 01:17:01 -0400

    Built skeleton model for /api/tasks routes, started router.js for the same

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,23 @@
 // build your `Task` model here
+
+const db = require('../../data/dbConfig')
+
+const {tasks, task_id} = require('../../data/databaseConstants')
+
+const getTask = async (id) => {
+    if (id) {
+        return db(tasks).where(task_id, id)
+    } else {
+        return db(projects)
+    }
+}
+
+const postTask = async (task) => {
+    const post = await db(tasks).insert(task)
+    return getTask(task)
+}
+
+module.exports = {
+    getTask,
+    postTask
+}
